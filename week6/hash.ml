99931ab450a51ccaa44615e5e6424b1579bb8557
Igor Gajsin <igajsin@mirantis.com>
2016-11-25 18:30:48 +0300

    finish Trie exercise!

--- week6/hash.ml
+++ week6/hash.ml
@@ -6,6 +6,8 @@ module type GenericTrie = sig
   val empty : unit -> 'a trie
   val insert : 'a trie -> string -> 'a -> 'a trie
   val lookup : 'a trie -> string -> 'a option
+  val print_int_trie : int trie -> unit
+  val add1 : 'a trie -> char -> 'a -> unit
 end;;
 
 module CharHashedType : Hashtbl.HashedType with type t = char =
@@ -41,33 +43,60 @@ struct
       match (trie, wlst) with
       | (Trie (x, _), []) -> x
       | (Trie (_, h), x::xs) -> lookup (CharHashtbl.find h x) xs in
-    lookup trie wlst;;
+    try
+      lookup trie wlst
+    with
+    |_ -> None;;
+
+  let add1 (Trie (_, h)) k v =
+  let value = Trie (Some v, CharHashtbl.create 0) in
+  CharHashtbl.add h k value;;
+
+  let rec print_int_trie (Trie (v, h)) =
+    let print_optional = function
+      | None -> print_string "None "
+      | Some x -> print_string ((string_of_int x) ^ " ") in
+    let rec print_hsh h =
+      if (CharHashtbl.length h = 0) then ()
+      else
+	CharHashtbl.iter
+	  (fun k vt ->
+	    begin
+              print_char ' ';
+	      print_char k;
+	      print_string ": ";
+              print_int_trie vt
+	    end
+	  )
+	  h in
+    begin
+      print_char '(';
+      print_optional v;
+      print_hsh h;
+      print_char ')';
+    end;;
 
   let insert trie w v =
     let wlst = string2char_list w in
-    let rec insert trie wlst =
-      match (trie, wlst) with
-      | (Trie (_, h), []) -> Trie (Some v, h)
-      | (Trie (k, h), x::xs) -> Trie (k, h)
-    in
-    insert trie wlst;;
-
-
-  let add1 trie k v =
-    let Trie (_, h1) = trie
-    and value = Trie (Some v, CharHashtbl.create 0) in
-    CharHashtbl.replace h1 k value;;
-
-  let print_trie (Trie (_, h)) =
-    let rec range low high = if low = high then [] else low::(range (low + 1) high) in
-    let all_char = List.map Char.chr (range 0 256) in
-    let print_node = function
-    | (x, Trie (y, _)) ->
-       match y with
-       | None -> (print_char x; print_string ": None\n")
-       | Some z -> (print_char x; print_string ": "; print_int z; print_string "\n") in
-    let keys = List.filter (fun c -> CharHashtbl.mem h c ) all_char in
-    let pairs = List.map (fun k -> (k, CharHashtbl.find h k)) keys in
-    List.iter print_node pairs;;
+    let rec insrt trie str v =
+      let mktrie v = Trie (Some v, CharHashtbl.create 0) in
+      match str with
+      | [] -> trie
+      | x::[] -> let (Trie (_, h)) = trie in
+                 begin
+                   CharHashtbl.replace h x (mktrie v);
+                   trie
+                 end
+      | x::xs ->
+         let (Trie (_, h)) = trie in
+         let trie1 = try CharHashtbl.find h x
+                     with |_ ->
+                            begin
+                              CharHashtbl.add h x (empty ());
+                              CharHashtbl.find h x
+                            end in
+         insrt trie1 xs v in
+    let (_) = insrt trie wlst v in
+    trie;;
 
 end;;
